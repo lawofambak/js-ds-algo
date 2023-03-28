a928a88d6e3c5465b18e8965a0fbc89ef76272ef
Alex Kim <lawofambak7@gmail.com>
2023-03-28 10:24:55 -0700

    Reverse an array

---
+++ arrayPractice.js
@@ -0,0 +1,19 @@
+// Function to reverse an array
+const array_1 = [1, 2, 3, 4, 5];
+
+function reverseArray(_array) {
+  let start = 0;
+  let end = _array.length - 1;
+
+  while (start < end) {
+    let temp = _array[start];
+    _array[start] = _array[end];
+    _array[end] = temp;
+    start++;
+    end--;
+  }
+
+  return _array;
+}
+
+reverseArray(array_1); // [5, 4, 3, 2, 1]
