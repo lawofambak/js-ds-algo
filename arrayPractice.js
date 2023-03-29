090279924a5d69d22be5b65802e558ac29971d89
Alex Kim <lawofambak7@gmail.com>
2023-03-29 11:17:19 -0700

    Min and Max values of an array

--- arrayPractice.js
+++ arrayPractice.js
@@ -18,3 +18,30 @@ function reverseArray(_array) {
 }
 
 reverseArray(array_1); // [5, 4, 3, 2, 1]
+
+// Function to get minimun and maximum values from an array
+// Time complexity: O(n)
+const array_2 = [1, 10, 7, 5, 11, 0];
+
+function getMinMax(_array) {
+  if (_array.length === 0) {
+    return;
+  } else if (_array.length === 1) {
+    return _array[0];
+  } else {
+    let min = _array[0];
+    let max = _array[1];
+
+    for (let i = 1; i < _array.length; i++) {
+      if (_array[i] < min) {
+        min = _array[i];
+      } else if (_array[i] > max) {
+        max = _array[i];
+      }
+    }
+
+    return { min, max };
+  }
+}
+
+getMinMax(array_2);
